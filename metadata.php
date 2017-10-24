e4142eb54846ac780898b9c34d82c5690feea94e
Tobias Merkl <merkl@proudsourcing.de>
2017-10-24 18:04:23 +0200

    commit 1

---
+++ metadata.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @copyright (c) Proud Sourcing GmbH | 2017
+ * @link www.proudcommerce.com
+ * @package psBlocks
+ * @version 2.0.0
+ **/
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.0';
+
+/**
+ * Module information
+ */
+$aModule = array(
+    'id' => 'psBlocks',
+    'title' => 'psBlocks',
+    'description' => array(
+        'de' => 'Verwalten von Oxid Template Blocks.',
+        'en' => 'Tool for managing Oxid template blocks'
+    ),
+    'thumbnail' => 'logo_pc-os.jpg',
+    'version' => '2.0.0',
+    'author' => 'Proud Sourcing GmbH',
+    'url' => 'https://www.proudcommerce.com/',
+    'email' => 'support@proudcommerce.com',
+    'extend' => array(
+
+    ),
+    'templates' => array(
+
+    ),
+    'blocks' => array(
+
+    ),
+    'settings' => array(
+
+    ),
+);
