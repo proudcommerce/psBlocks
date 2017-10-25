2fe156bdd32d70d886dda8d1a9f24813be16ca39
Tobias Merkl <merkl@proudsourcing.de>
2017-10-25 10:24:55 +0200

    commit 7

---
+++ Application/views/admin/de/psblocks_lang.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @copyright (c) Proud Sourcing GmbH | 2017
+ * @link www.proudcommerce.com
+ * @package psBlocks
+ * @version 2.0.0
+ **/
+
+$sLangName = 'Deutsch';
+
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = array(
+    'charset' => 'UTF-8',
+
+    'NAVIGATION_PSMODULE' => 'Proud Sourcing',
+    'psblocks' => 'psBlocks',
+
+    'psblocks_tplblockslist' => 'Template-Blöcke',
+    'psblocks_tplblocksmain' => 'Stamm',
+    'PSBLOCKS_ACTIVATE_DEACTIVATE' => 'Block aktiv/inaktiv (1/0)',
+    'PSBLOCKS_ACTIVATE' => 'Block aktivieren',
+    'PSBLOCKS_DEACTIVATE' => 'Block deaktivieren',
+    'PSBLOCKS_MANAGE' => 'bearbeiten',
+    'PSBLOCKS_OXBLOCKS' => 'Block',
+    'PSBLOCKS_OXMODULE' => 'Modul',
+    'PSBLOCKS_OXTEMPLATE' => 'Template',
+    'PSBLOCKS_OXPOS' => 'Sortierung',
+    'PSBLOCKS_OXFILE' => 'Datei',
+
+);
